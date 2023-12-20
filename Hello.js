53e13c74e54a2eb79d756e0a8d4e7a23ac0c486d
ukamaka203 <154434643+ukamaka203@users.noreply.github.com>
2023-12-20 08:28:43 -0800

    Add files via upload

---
+++ Hello.js
@@ -0,0 +1,9 @@
+ const name = "Ukamaka";
+ 
+ const displayMessage= ()=>{
+     return "I need help!";
+ };
+ function Hello (){
+    return <h1>The Message is:!{displayMessage()}</h1>;
+}
+export default Hello
